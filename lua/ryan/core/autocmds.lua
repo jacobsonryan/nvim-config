942ae16e02a43fa001e22d05c980efa99557ac7b
jacobsonryan <ryanhjacobson@gmail.com>
2025-04-23 19:12:36 -0400

    fix autocmd

--- lua/ryan/core/autocmds.lua
+++ lua/ryan/core/autocmds.lua
@@ -1,23 +1,22 @@
--- Auto-quit Neovim when only NvimTree is left or when quitting from inside it
 local augroup = vim.api.nvim_create_augroup("NvimTreeAutoQuit", { clear = true })
 
--- Quit when only NvimTree is left
 vim.api.nvim_create_autocmd("BufEnter", {
   group = augroup,
   pattern = "NvimTree_*",
   callback = function()
     local layout = vim.fn.winlayout()
-    local win = layout[2]
-    local is_single_window = layout[1] == "leaf" and layout[3] == nil
-    local is_nvimtree = vim.bo[vim.api.nvim_win_get_buf(win)].filetype == "NvimTree"
+    if layout[1] == "leaf" then
+      local win = vim.api.nvim_get_current_win()
+      local buf = vim.api.nvim_win_get_buf(win)
+      local is_nvimtree = vim.bo[buf].filetype == "NvimTree"
 
-    if is_single_window and is_nvimtree then
-      vim.cmd("confirm quit")
+      if is_nvimtree then
+        vim.cmd("confirm quit")
+      end
     end
   end,
 })
 
--- Quit all if you quit from within NvimTree
 vim.api.nvim_create_autocmd("QuitPre", {
   group = augroup,
   callback = function()
